e918f427d6a09c9b66ac396677802d66838faf48
Ozair Khan <ozairk048@gmail.com>
2026-03-01 00:18:36 +0500

    Update terraform.tf

--- MyTerrafromModule/terraform.tf
+++ MyTerrafromModule/terraform.tf
@@ -8,11 +8,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "via-terraform-state-bucket-2026"
-    key            = "dev/module/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "my-terraform-state-dynameoDb-2026"
-    encrypt        = true
-  }
+  #backend "s3" {
+   # bucket         = "via-terraform-state-bucket-2026"
+    #key            = "dev/module/terraform.tfstate"
+    #region         = "us-east-1"
+    #dynamodb_table = "my-terraform-state-dynameoDb-2026"
+    #encrypt        = true
+  # }
 }
+
