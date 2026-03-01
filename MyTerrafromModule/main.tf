447bc0d803931d45a83758894cf1b422e9218f38
Ozair Khan <ozairk048@gmail.com>
2026-03-01 22:23:19 +0500

    Update main.tf

--- MyTerrafromModule/main.tf
+++ MyTerrafromModule/main.tf
@@ -1,7 +1,29 @@
-# Either Put the values directly or provide values inside .tfvars which is industrial approach
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.34.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "bucket-for-diff-terraform-worksapces"
+    dynamodb_table = "dynamodb-for-diff-terraform-worksapces"
+    key            = "demo/terraform.tfstate" # The path create at s3 will be: s3_name/env/workspace_name/demo/.tfstate
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
 module "myModule" {
+  # Either Put the values directly or provide values inside .tfvars which is industrial approach 
+
   # The Module is called from Github Repo (main branch) 
-  source = "git::https://github.com/OzairKhan1/Terraform.git//MyTerrafromModule/GenModule?ref=main"
+  source = "git::https://github.com/OzairKhan1/Terraform.git//MyTerrafromModule/GenModule?ref=dev"
   environment  = var.environment
   project_name = var.project_name
   path_to_public_key = var.path_to_public_key
@@ -16,4 +38,5 @@ module "myModule" {
     ManagedBy   = "Terraform"
   }
 }
+
 
